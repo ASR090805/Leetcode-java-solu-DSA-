2c3618f97726e63f2420751fe559d2e6dbe6d097
Anuj Rawat <rawatanuj8126@gmail.com>
2026-09-06 21:43:35 +0530

    Time: 1 ms (99.94%), Space: 47.3 MB (73.6%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -6,7 +6,7 @@ class Solution {
             i++;
         }
         if(i==0 || i==arr.length-1)return false;
-        while(i<arr.length-1 && arr[i]>arr[i+1]){
+        while(i+1<arr.length && arr[i]>arr[i+1]){
             i++;
         }
         return (i==arr.length-1);
